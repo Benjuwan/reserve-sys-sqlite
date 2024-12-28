45fddfcfcede225091c82841c626ebb3bbdc5496
benjuwan <benjuwan.banquet12@gmail.com>
2024-12-28 10:56:31 +0900

    fix:Reactのハイドレーションエラー418対策.

--- src/app/components/common/components/TheHeadingOne.tsx
+++ src/app/components/common/components/TheHeadingOne.tsx
@@ -1,17 +1,24 @@
 "use client"
 
 import { usePathname } from "next/navigation";
-import { memo, useMemo } from "react";
+import { memo, useEffect, useState } from "react";
 
 function TheHeadingOne() {
     const pathName: string = usePathname();
-    const today: string = useMemo(() => new Date().toLocaleDateString(), []);
+
+    /* React のハイドレーションエラー(418) https://react.dev/errors/418?args[] 対策 */
+    const [theToday, setToday] = useState<string>('');
+    useEffect(() => {
+        const today: string = new Date().toLocaleDateString();
+        setToday(today);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <>
             <h1>Reservation Rooms</h1>
             {pathName.length === 1 &&
-                <p>- {today}の予約内容</p>
+                <p>- {theToday}の予約内容</p>
             }
         </>
     );
