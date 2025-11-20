b4720b0fcb04c1860dad37df80ea8c0cf42225c9
benjuwan <benjuwan.banquet12@gmail.com>
2025-11-20 10:14:37 +0900

    fix: 各種ライブラリのアップデート・アップグレード及びREADMEの更新、リファクタリング.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" data-scroll-behavior="smooth">
+    <html lang="ja">
       <body>
         <Header />
         <div className="globalWrapper">
